7b45c2517da87d209b917d43d5b779f6ec4e31d5
soycaringal <soycaringal@github.com>
2015-10-13 12:36:41 +0800

    update test to phantomjs

--- tests/simpleTest.php
+++ tests/simpleTest.php
@@ -10,7 +10,7 @@ class simpleTestTest extends PHPUnit_Extensions_Selenium2TestCase
      */
     public function setUp()
     {
-        $this->setBrowser('firefox');
+        $this->setBrowser('phantomjs');
         $this->setHost('127.0.0.1');
         $this->setPort(4444);
         $this->setBrowserUrl('https://www.google.com.ph/');
