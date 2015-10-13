7063897ed56b6532999c76305f307d3899a48ebe
soycaringal <soycaringal@github.com>
2015-10-13 12:33:28 +0800

    Update simpleTest.php

--- tests/simpleTest.php
+++ tests/simpleTest.php
@@ -10,37 +10,21 @@ class simpleTestTest extends PHPUnit_Extensions_Selenium2TestCase
      */
     public function setUp()
     {
-        $this->setBrowser('phantomjs');
+        $this->setBrowser('firefox');
         $this->setHost('127.0.0.1');
         $this->setPort(4444);
-        $this->setBrowserUrl('http://eden.dev/');
+        $this->setBrowserUrl('https://www.google.com.ph/');
     }
     
     /** 
-     * Method test 
-     *
+     * Method testSelenium 
      * @test 
-     * @return void 
      */ 
-    public function test()
+    public function testSelenium()
     {
-        $this->url("/control/login");
-        $this->byName("auth_slug")->value("admin@openovate.com");
-        $this->byName("auth_password")->value("admin");
-        $this->byCssSelector("button.btn.btn-primary")->click();
-        $this->byLinkText("Create New App")->click();
-        $this->byName("app_name")->value("Test2");
-        $this->byName("app_domain")->value("test.com");
-        $this->byName("app_website")->value("http://test.com");
-        $this->byName("app_permissions[]")->click();
-        $this->byXPath("(//input[@name='app_permissions[]'])[2]")->click();
-        $this->byCssSelector("button.btn.btn-primary")->click();
-        $this->byCssSelector("i.fa.fa-lock")->click();
-        $this->byXPath("(//button[@name='action'])[2]")->click();
-        $this->byCssSelector("i.fa.fa-edit")->click();
-        $this->byName("app_domain")->value("test2.com");
-        $this->byCssSelector("button.btn.btn-primary")->click();
-        // $this->byLinkText("Logout")->click();
+        $this->url("/?gfe_rd=cr&ei=UIkcVsvsOsuM8QfA4ZzYDw&gws_rd=ssl");
+        $this->byName("btnI")->click();
+        $this->byId("searchinput")->value("aw");
     }
 }
 ?>
